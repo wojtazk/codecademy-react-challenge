91102b0c3076cb1f39aef3ed3af5b20f1600986d
Wojciech Kowal <w.kowal202@gmail.com>
2023-02-22 19:31:44 +0100

    Implement tile component

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React from 'react';
 
-export const Tile = () => {
+export const Tile = ({ item }) => {
+  let index = 0;
+  let content = [];
+
+  for (const [key, value] of Object.entries(item)) {
+    let className = 'tile';
+    if (key === 'name' || key === 'contact') className += '-title';
+
+    content.push(
+      <p className={className} key={index.toString()}>
+        {value}
+      </p>
+    );
+
+    index++;
+  }
   return (
     <div className="tile-container">
-      
+      {/* <p className="tile-title">{item?.name || item?.contact.name}</p>
+
+      <p className="tile">{item.phoneNumber}</p>
+      <p className="tile">{item.email}</p> */}
+      {content}
     </div>
   );
 };
