889d8bd04cb7f05221fa1da143b9186d417d1478
Wojciech Kowal <w.kowal202@gmail.com>
2023-02-22 14:51:23 +0100

    Add new contact form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 export const ContactForm = ({
   name,
@@ -7,9 +7,39 @@ export const ContactForm = ({
   setPhone,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit,
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        required
+        id="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <label htmlFor="phone">Phone</label>
+      <input
+        type="tel"
+        required
+        pattern="[+]?[0-9]{2}[ -]{1}[0-9]{3}[ -]{1}[0-9]{3}[ -]{1}[0-9]{3}"
+        title="Accepted formats: (+12 123 123 123), (12 123 123 123), (+12-123-123-123)"
+        id="phone"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+      />
+
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input type="submit" value="Add contact" />
+    </form>
   );
 };
